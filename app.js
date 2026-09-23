0be4be27bf29a330c7ef292cf42c5710947be143
Martin Kilp <martin.kilp@tptlive.ee>
2026-09-23 23:54:49 +0300

    @media style & respponsivness

--- app.js
+++ app.js
@@ -5,18 +5,27 @@ function toggleSidebar() {
     sidebar.classList.toggle('close')
     toggleButton.classList.toggle('rotate')
 
+    closeAllSubMenus()
+}
+
+function toggleSubmenu(button) {
+
+    if(!button.nextElementSibling.classList.contains('show')){
+        closeAllSubMenus()
+    }
+
+    button.nextElementSibling.classList.toggle('show')
+    button.classList.toggle('rotate')
+
+    if (sidebar.classList.contains('close')) {
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
+    }
+}
+
+function closeAllSubMenus() {
     Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
         ul.classList.remove('show')
         ul.previousElementSibling.classList.remove('rotate')
     })
-
-}
-
-function toggleSubmenu(button) {
-    button.nextElementSibling.classList.toggle('show')
-
-    if(sidebar.classList.contains('close')){
-        sidebar.classList.toggle('close')
-        toggleButton.classList.toggle('rotate')
-    }
 }
